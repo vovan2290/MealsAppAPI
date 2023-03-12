f9873e721e935ac17a3220b422ac9befe43665b9
Volodymyr <vovhavgtr@gmail.com>
2023-03-11 19:43:15 -0500

    Initial Commit

---
+++ MealsAppAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealsAppAPI
+//
+//  Created by Imac on 3/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
